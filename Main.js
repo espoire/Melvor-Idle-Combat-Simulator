ca2c51f5600d05b71f266d216d84859757d5d4dc
espoire <a.pergrossi@gmail.com>
2021-08-03 23:24:23 -0400

    Added TabPanel

--- Main.js
+++ Main.js
@@ -2,18 +2,19 @@ import { renderCalculationsTo, renderFindMonsterTo, renderSlayerSummaryTo } from
 import appendMonsterOptions, { appendMonsterInputs, onMonsterOptionSelected } from "./scripts/ui/MonsterUI.js";
 import { appendOptionsInputs } from "./scripts/ui/OptionsUI.js";
 import { appendPlayerInputs } from "./scripts/ui/PlayerUI.js";
+import TabPanel from "./scripts/ui/widgets/TabPanel.js";
 
 window.onCalculateButtonClick = function () {
     renderCalculationsTo(document.getElementById('calculations'));
-    document.getElementById('calculate-output').style.display = 'inline-block';
+    // document.getElementById('calculate-output').style.display = 'inline-block';
 };
 window.onFindMonsterButtonClick = function () {
     renderFindMonsterTo(document.getElementById('found-monster'));
-    document.getElementById('find-monster-output').style.display = 'inline-block';
+    // document.getElementById('find-monster-output').style.display = 'inline-block';
 }
 window.onShowSlayerButtonClick = function () {
     renderSlayerSummaryTo(document.getElementById('slayer-table'));
-    document.getElementById('show-slayer-output').style.display = 'inline-block';
+    // document.getElementById('show-slayer-output').style.display = 'inline-block';
 }
 
 window.onMonsterSelected = onMonsterOptionSelected;
@@ -23,4 +24,13 @@ window.onMonsterSelected = onMonsterOptionSelected;
     appendMonsterOptions(document.getElementById('monster-select'));
     appendMonsterInputs(document.getElementById('monster-inputs'));
     appendOptionsInputs(document.getElementById('options'));
+
+    new TabPanel({
+        parent: 'output',
+        tabs: [
+            'calculate-output',
+            'find-monster-output',
+            'show-slayer-output'
+        ]
+    });
 })();
---
+++ scripts/ui/widgets/TabPanel.js
@@ -0,0 +1,95 @@
+/**
+ * Expects an array of HTMLElements to manage.
+ * Each array element may be:
+ *    - a string
+ *          The ID of the element, not including the #.
+ *    - an HTMLElement reference
+ *          The actual element itself to manage
+ *    - a config object, with any of the following keys:
+ *        - element: string | HTMLElement
+ *              The element, as the above non-object options.
+ *        - label: string
+ *              The text to display to the user for this tab.
+ */
+
+export default class TabPanel {
+    /**
+     * 
+     * @param {object} config
+     * @param {(string | !HTMLElement)} config.parent
+     * @param {(string | !HTMLElement | { label: string, element: (string | !HTMLElement)})[]} config.tabs
+     */
+    constructor(config) {
+        TabPanel._validateConfig(config);
+
+        this.parent = TabPanel._getHtmlElementReference(config.parent);
+
+        this.tabs = config.tabs.map(tab => {
+            tab = TabPanel._getHtmlElementReference(tab);
+
+            if(tab instanceof HTMLElement)
+                return {element: tab};
+
+            tab.element = TabPanel._getHtmlElementReference(tab.element);
+            return tab;
+        });
+        this.tabs.forEach(tab =>
+            tab.label = tab.label || tab.element.id
+        )
+
+        this.render();
+    }
+
+    render() {
+        const tabPanelWrapperDivElement = document.createElement('div');
+        tabPanelWrapperDivElement.className = 'panel-wrapper vbox';
+
+        const tabsBarDivElement = document.createElement('div');
+        tabsBarDivElement.className = 'panel-title tab-bar hbox';
+
+        const tabPanelBodyDivElement = document.createElement('div');
+        tabPanelBodyDivElement.className = 'panel-body';
+
+        this.tabs.forEach(tab => {
+            tab.element.style.display = 'none';
+            tabPanelBodyDivElement.appendChild(tab.element);
+
+            tab.button = document.createElement('button');
+            tab.button.className = 'tab-button';
+            tab.button.innerHTML = tab.label;
+
+            const self = this;
+            tab.button.onclick = function () {
+                self.activateTab(tab);
+            };
+
+            tabsBarDivElement.appendChild(tab.button);
+        });
+
+        this.activateTab(this.tabs[0]);
+
+        tabPanelWrapperDivElement.appendChild(tabsBarDivElement);
+        tabPanelWrapperDivElement.appendChild(tabPanelBodyDivElement);
+        this.parent.appendChild(tabPanelWrapperDivElement);
+    }
+
+    activateTab(tab) {
+        this.tabs.forEach(tab =>
+            tab.element.style.display = 'none'
+        );
+
+        tab.element.style.display = 'inherit';
+    }
+
+    static _validateConfig(config) {
+        if(!config.parent || !config.tabs) throw new Error();
+        if(typeof parent != 'string' && ! parent instanceof HTMLElement) throw new Error();
+        if(!Array.isArray(config.tabs)) throw new Error();
+    }
+
+    static _getHtmlElementReference(element) {
+        if(typeof element == 'string')
+            return document.getElementById(element);
+        return element;
+    }
+}
